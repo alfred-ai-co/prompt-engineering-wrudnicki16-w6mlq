4370b43eaa22b1f12a53e5d659b4fca30d2af714
wrudnicki16 <wyattrudnicki@gmail.com>
2024-10-03 17:09:32 -0700

    Add zero shot manual format_instructions

--- prompts/zero_shot.py
+++ prompts/zero_shot.py
@@ -1,5 +1,25 @@
 ZERO_SHOT_PROMPT = """
-Given the following topic {input}, generate a quiz with a randon number of questions between 1 to 5.
+Given the following topic {input}, generate a quiz with 5 questions increasing from beginner to expert in difficulty level.
 
-{format_instructions}
+- a) <choice_1>
+- b) <choice_2>
+- c) <choice_3>
+- d) <choice_4>
+- Answer: <single_character_key>
+
+The output should be a JSON Quiz object with the following structure:
+{{
+  "questions": [
+    {{
+      "question": "<question_text>",
+      "choices": [
+        {{"key": "a", "value": "<choice_1>"}},
+        {{"key": "b", "value": "<choice_2>"}},
+        {{"key": "c", "value": "<choice_3>"}},
+        {{"key": "d", "value": "<choice_4>"}}
+      ],
+      "answer": "<single_character_key>"
+    }}
+  ]
+}}
 """
